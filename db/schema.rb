c9dfc7d2c94c9c00c3b9d50068f9d7d5b4638c0b
Mohamed Ibrahim <mohamed.ibrahim1@uoit.net>
2018-08-16 01:13:00 +0200

    added models

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_06_09_095316) do
+ActiveRecord::Schema.define(version: 2018_08_15_225000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "attachinary_files", id: :serial, force: :cascade do |t|
-    t.string "attachinariable_type"
-    t.integer "attachinariable_id"
-    t.string "scope"
-    t.string "public_id"
-    t.string "version"
-    t.integer "width"
-    t.integer "height"
-    t.string "format"
-    t.string "resource_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
-  end
-
-  create_table "cocktails", id: :serial, force: :cascade do |t|
+  create_table "cocktails", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "doses", id: :serial, force: :cascade do |t|
+  create_table "doses", force: :cascade do |t|
     t.string "description"
-    t.integer "cocktail_id"
-    t.integer "ingredient_id"
+    t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
-  create_table "ingredients", id: :serial, force: :cascade do |t|
+  create_table "ingredients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
